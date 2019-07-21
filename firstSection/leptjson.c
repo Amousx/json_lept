a7fe3228a2924a54313d1247c858bfce6a0a56a9
amosxh <amosxh2018@gmail.com>
2019-07-21 20:56:09 +0800

    第一题

--- firstSection/leptjson.c
+++ firstSection/leptjson.c
@@ -20,49 +20,31 @@ static int lept_parse_null(lept_context* c, lept_value* v) {
     if (c->json[0] != 'u' || c->json[1] != 'l' || c->json[2] != 'l')
         return LEPT_PARSE_INVALID_VALUE;
     c->json += 3;
-    if(*c->json =='\0'){
-        v->type = LEPT_NULL;
-        return LEPT_PARSE_OK;
-    }
-    else if(*c->json == ' '){
-        lept_parse_whitespace(c);
-        if(*c->json == '\0') return LEPT_PARSE_INVALID_VALUE;
-        else{
-            v->type = LEPT_NULL;
-            return LEPT_PARSE_ROOT_NOT_SINGULAR;
-        }
-    }
-}
-
-static int lept_parse_root_not_singular(lept_context* c, lept_value* v){
-    while(*c->json != ' ' || *c->json != '\0'){
-        c->json++;
-    }
-    
-    if(*c->json == ' '){
-        lept_parse_whitespace(c);
-    }
-    if(*c->json == '\0') return LEPT_PARSE_INVALID_VALUE;
     v->type = LEPT_NULL;
-    return LEPT_PARSE_ROOT_NOT_SINGULAR;
+    return LEPT_PARSE_OK;
 }
 
 static int lept_parse_value(lept_context* c, lept_value* v) {
     switch (*c->json) {
-        case 'n':  return lept_parse_null(c, v);    //空
-        case '\0': return LEPT_PARSE_EXPECT_VALUE;  //纯空白
-        //default:   return LEPT_PARSE_INVALID_VALUE; //其他情况
-        default: return lept_parse_root_not_singular(c,v);
+        case 'n':  return lept_parse_null(c, v);
+        case '\0': return LEPT_PARSE_EXPECT_VALUE;
+        default:   return LEPT_PARSE_INVALID_VALUE;
     }
 }
 
 int lept_parse(lept_value* v, const char* json) {
+    int ret;
     lept_context c;
     assert(v != NULL);
     c.json = json;
     v->type = LEPT_NULL;
     lept_parse_whitespace(&c);
-    return lept_parse_value(&c, v);
+    if((ret = lept_parse_value(&c, v)) == LEPT_PARSE_OK){
+        lept_parse_whitespace(&c);
+        if(*c.json != '\0')
+            return LEPT_PARSE_ROOT_NOT_SINGULAR;
+    }
+    return ret;
 }
 
 lept_type lept_get_type(const lept_value* v) {
