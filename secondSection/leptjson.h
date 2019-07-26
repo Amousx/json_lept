08d43be697cabb16a664fb07cdb0783ef61e3f45
Amousx <amosxh2018@gmail.com>
2019-07-26 16:19:48 +0800

    asset get_number

---
+++ secondSection/leptjson.h
@@ -0,0 +1,31 @@
+#ifndef LEPTJSON_H__
+#define LEPTJSON_H__
+
+typedef enum{
+    LEPT_NULL,
+    LEPT_TRUE,
+    LEPT_FALSE,
+    LEPT_NUMEBR,
+    LEPT_STRING,
+    LEPT_ARRAY,
+    LEPT_OBJECT
+}lept_type;
+
+typedef struct{
+    lept_type type;
+    double n;
+}lept_value;    //tree struct ,just like the node
+
+typedef enum{
+    LEPT_PARSE_OK = 0,                   //正常返回
+    LEPT_PARSE_EXPECT_VALUE,             //json只有空白
+    LEPT_PARSE_INVALID_VALUE,            //非其他情况
+    LEPT_PARSE_ROOT_NOT_SINGULAR         //空白之后还有词
+}lept_error_type;    //enum return types
+
+int lept_parse(lept_value * v,const char* json);
+
+lept_type lept_get_type(const lept_value* v);   //return the type of the node
+
+double lept_get_number(const lept_value* v);
+#endif     /*LEPTJSON_H__*/
